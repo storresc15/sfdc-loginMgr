be7ab6f7e73f692c5e11ba1359ec2b8378e8375d
Sebas <sebas.torres.88@gmail.com>
2022-01-19 21:18:19 -0600

    Finished the component to be updated with the right configuration in xml meta file

--- force-app/main/default/lwc/logInToExperienceBanner/logInToExperienceBanner.js
+++ force-app/main/default/lwc/logInToExperienceBanner/logInToExperienceBanner.js
@@ -1,22 +1,27 @@
-import { LightningElement, wire, track } from "lwc";
+import { LightningElement, api, wire, track } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
-import USER_ID from "@salesforce/user/id";
+import USER_ID from "@salesforce/user/Id";
 import NAME_FIELD from "@salesforce/schema/User.Name";
+import USERNAME_FIELD from "@salesforce/schema/User.Username";
 import getSessionInfo from "@salesforce/apex/LogInToExperienceManagementController.getSessionInfo";
 
 export default class LogInToExperienceBanner extends LightningElement {
   //Private Variables
   @track isLoggedInAsUser = false;
   @track name;
+  @track username;
+  @api displayUserInfo;
+  @api displayUser;
 
   //Wire functionas
   @wire(getRecord, {
     recordId: USER_ID,
-    fields: [NAME_FIELD]
+    fields: [NAME_FIELD, USERNAME_FIELD]
   })
   wireuser({ data }) {
     if (data) {
       this.name = data.fields.Name.value;
+      this.username = data.fields.Username.value;
     }
   }
 
